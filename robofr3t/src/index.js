8f5d06eae100e7270598bb1bed1d3833acfac9be
Tin Tran <trantrongtinlhp@gmail.com>
2018-05-08 10:58:59 +0700

    simple robot app

--- robofr3t/src/index.js
+++ robofr3t/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Card from './Card';
 import App from './App';
+import {robots} from './robots';
+import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('App'));
+ReactDOM.render(<Card robots={robots}/>, document.getElementById('Card'));
+console.log(robots.length);
 registerServiceWorker();
